c24b07d31af44982743f8bfabea4ce2333e6052f
Gilbert R. Canedo Jr <143938021+gilberx@users.noreply.github.com>
2023-11-15 20:18:56 +0800

    update2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import './App.css';
 import LandingPage from './LandingPage.jsx'
 
@@ -8,25 +6,6 @@ function App() {
     <div className="App">
       <LandingPage></LandingPage>
     </div>
-=======
-import './App.css';
-import Login from './Login'
-import Register from './Register'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/register" element={<Register />} /> */}
-        <Route path="/" element={<Register />} />
-      </Routes>
-    </Router>
-    
->>>>>>> main
   );
 }
 
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['Join a Class', 'Courses', 'How it Works', 'About Us'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
@@ -51,10 +51,10 @@ function ResponsiveAppBar() {
                 display: { xs: 'none', md: 'flex' },
                 fontFamily: 'Inter, sans-serif',
                 fontWeight: '600',
-                letterSpacing: '.3rem',
                 color: 'inherit',
+                fontSize: '30px',
                 textDecoration: 'none',
-                mr: 2,
+                mr: 80,
               }}
             >
               CodeTech
@@ -106,65 +106,96 @@ function ResponsiveAppBar() {
                 mr: 2,
                 display: { xs: 'flex', md: 'none' },
                 flexGrow: 1,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
+                fontFamily: 'Inter, sans-serif',
+                fontWeight: '600',
+                color: 'inherit',
+                fontSize: '30px',
                 color: 'inherit',
                 textDecoration: 'none',
               }}
             >
-              LOGO
+              CodeTech
             </Typography>
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
               {pages.map((page) => (
                 <Button
                   key={page}
                   onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  sx={{fontFamily: 'Inter, sans-serif',
+                  color: 'inherit',
+                  fontSize: '18px',
+                  my: 2, 
+                  color: 'white', 
+                  display: 'block', 
+                  mr: 6,
+                  textTransform: 'none' }}
                 >
                   {page}
                 </Button>
               ))}
             </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+            <Box>
+                <Button onClick={handleCloseNavMenu}
+                sx={{
+                  color: 'white',
+                  display: 'block',
+                  mr: 4,
+                  border: '2px solid white',
+                  borderRadius: '25px',
+                  width: '150px',
+                  height: '50px',
+                  textTransform: 'none'
+                }}>
+                  Sign Up
+                </Button>
+            </Box>
+            <Box>
+                <Button onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  backgroundColor: '#458C83',
+                  color: 'white',
+                  display: 'block',
+                  borderRadius: '25px',
+                  width: '150px',
+                  height: '50px',
+                  textTransform: 'none'
+                }}>
+                  Login
+                </Button>
             </Box>
           </Toolbar>
         </Container>
       </AppBar>
     </div>
-    <div>
-      <p style={{ color: '#353535', fontFamily: 'Montserrat, sans-serif', fontWeight: 800, fontSize: 100 }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <p style={{ color: '#353535', fontFamily: 'Montserrat, sans-serif', fontWeight: 800, fontSize: 120, textAlign: 'center' }}>
         Code learning<br></br>
         just got better!
       </p>
+      <p style={{ color: '#353535', fontFamily: 'Inter, sans-serif', fontSize: 39, textAlign: 'center', marginTop: '-60px' }}>
+      Empowering coders, one lesson at a time.<br></br>Join us! It's free.
+      </p>
+    </div>
+    <div style={{marginTop: '180px',display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '80px'}}>
+      <Button style={{
+                  my: 2,
+                  backgroundColor: '#F5FFFD',
+                  color: '#212121',
+                  display: 'block',
+                  borderRadius: '35px',
+                  width: '300px',
+                  height: '70px', 
+                  fontFamily: 'Inter, sans-serif',
+                  fontWeight: 600,
+                  fontSize: 22,
+                  textTransform: 'none',
+                }}>
+        Start learning now!
+      </Button>
+    </div>
+    <div>
+        <Box sx={{ bgcolor: '#212121', height: '100vh', borderRadius: '40px' }} />
     </div>
   </div>
   );

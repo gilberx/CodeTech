0d42a34b60d0c685a89354c33ccc96fc46f34da2
shannandreii <143739759+shannandreii@users.noreply.github.com>
2023-11-21 00:02:42 +0800

    Forgot Password Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import Login from './Login'
-import Register from './Register'
+import ForgotPassword from './Login/ForgotPassword';
+import Login from './Login/Login'
+import ResetPassword from './Login/ResetPassword';
+import Register from './Register/Register'
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -12,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         {/* <Route path="/register" element={<Register />} /> */}
         <Route path="/" element={<Register />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
+        <Route path="/reset" element={<ResetPassword />} />
       </Routes>
     </Router>
     

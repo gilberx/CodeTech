ec386a34b6446fcdd9fa63063b0999e6a13844f7
shannandreii <143739759+shannandreii@users.noreply.github.com>
2023-11-21 16:56:45 +0800

    unnecessary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import LandingPage from './LandingPage.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Register/Register.js';
 import Login from './Login/Login.js';
+import ForgotPassword from './Login/ForgotPassword.js';
+import ResetPassword from './Login/ResetPassword.js';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />}/>
+        <Route path="/forgot" element={<ForgotPassword />}/>
+        <Route path="/getcode" element={<ResetPassword />}/>
       </Routes>
     </Router>
   );
